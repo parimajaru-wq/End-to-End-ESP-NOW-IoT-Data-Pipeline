4afdbdab847a08bf5d4cf5f82b80d0ec373e0b5c
parimajaru-wq <parima.jaru@gmail.com>
2026-05-09 23:26:41 +0700

    Delete credentials.h

--- credentials.h
+++
@@ -1,4 +0,0 @@
-const char* ssid =        "Kopy";            // Wifi or Hotspot SSID
-const char* password =    "12345678";        // Wifi or Hotspot password
-const char* mqtt_user =   "iot-106";   // MQTT broker or EMQX username
-const char* mqtt_pass =   "106";   // MQTT broker or EMQX password
